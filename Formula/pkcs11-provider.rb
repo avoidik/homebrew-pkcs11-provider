c9d9b23a12e2ea1c6b378c0f4ef45f14a6d2a28a
Viacheslav Vasilyev <avoidik@gmail.com>
2024-05-29 23:35:42 +0100

    bump version

--- Formula/pkcs11-provider.rb
+++ Formula/pkcs11-provider.rb
@@ -1,8 +1,8 @@
 class Pkcs11Provider < Formula
   desc "Openssl 3.x provider to access software or hardware tokens via PKCS#11 interface"
   homepage "https://github.com/latchset/pkcs11-provider"
-  url "https://github.com/latchset/pkcs11-provider/releases/download/v0.3/pkcs11-provider-0.3.tar.xz"
-  sha256 "72275ddf59787c810d13c6edc756ba57db014ca9d1613e09fd22302b7a725216"
+  url "https://github.com/latchset/pkcs11-provider/releases/download/v0.4/pkcs11-provider-0.4.tar.xz"
+  sha256 "16869f5cf0aee61545957e5106b6263fa74b1cb949a11fea4d54ec83c34431fc"
   license "Apache-2.0"
   head "https://github.com/latchset/pkcs11-provider.git", branch: "main"
 
@@ -11,21 +11,17 @@ class Pkcs11Provider < Formula
     strategy :github_latest
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl@3"
 
   def install
-    args = %W[
-      --prefix=#{prefix}
-      --with-openssl=#{Formula["openssl@3"].opt_prefix}
-    ]
+    inreplace "src/meson.build", "install_dir: provider_path,", "install_dir: '#{lib}/pkcs11-provider'"
 
-    system "autoreconf", "-fiv"
-    system "./configure", *args
-    system "make", "install"
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
   end
 
   def caveats
