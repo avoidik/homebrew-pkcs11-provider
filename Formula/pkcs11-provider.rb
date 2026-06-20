8429e094044ab0896b6d98e67f22efd5b19b3e50
Viacheslav Vasilyev <avoidik@gmail.com>
2026-06-20 10:34:22 +0100

    bump version

--- Formula/pkcs11-provider.rb
+++ Formula/pkcs11-provider.rb
@@ -1,11 +1,11 @@
 class Pkcs11Provider < Formula
   desc "Openssl 3.x provider to access software or hardware tokens via PKCS#11 interface"
-  homepage "https://github.com/latchset/pkcs11-provider"
-  version "0.6"
-  url "https://github.com/latchset/pkcs11-provider/releases/download/v#{version}/pkcs11-provider-#{version}.tar.xz"
-  sha256 "f20e85c1e1ae819ddb440bdba47fa3b44943aaebaf05e59b04630126497a6ed6"
+  homepage "https://github.com/openssl-projects/pkcs11-provider"
+  version "1.2.0"
+  url "https://github.com/openssl-projects/pkcs11-provider/releases/download/v#{version}/pkcs11-provider-#{version}.tar.xz"
+  sha256 "36a2f13859f3e2a9c74d1d4064f8d406689b0201e25968aba952010ed73bfec2"
   license "Apache-2.0"
-  head "https://github.com/latchset/pkcs11-provider.git", branch: "main"
+  head "https://github.com/openssl-projects/pkcs11-provider.git", branch: "main"
 
   livecheck do
     url :stable
@@ -32,7 +32,7 @@ class Pkcs11Provider < Formula
       Alternatively, you may set the provider via the environment variable as follows:
         export PKCS11_PROVIDER_MODULE="#{lib}/pkcs11.dylib"
       Check official guide for more details:
-        https://github.com/latchset/pkcs11-provider/blob/main/HOWTO.md
+        https://github.com/openssl-projects/pkcs11-provider/blob/main/HOWTO.md
     EOS
   end
 
